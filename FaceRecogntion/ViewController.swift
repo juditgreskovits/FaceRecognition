24c22c3eea0ec99d97806a16b726ab5b28512a2d
JUDIT GRESKOVITS <juditgreskovits@JUDITs-MacBook-Air.local>
2014-10-26 23:50:28 +0000

    progress

--- FaceRecogntion/ViewController.swift
+++ FaceRecogntion/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureMetadataOutputObjectsDelegate {
+    
+    let _serialQueue = dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    
+    var _captureSession: AVCaptureSession?
+    var _facesMetadataObjects: NSArray?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,34 +36,95 @@ class ViewController: UIViewController {
             captureSession.sessionPreset = AVCaptureSessionPresetHigh
         }
         
+        // Video data output
+        
         var videoDataOutput = createVideoDataOutput()
+        if(captureSession.canAddOutput(videoDataOutput)) {
+            captureSession.addOutput(videoDataOutput)
+            let connection: AVCaptureConnection = videoDataOutput.connections[0] as AVCaptureConnection
+            connection.videoOrientation = AVCaptureVideoOrientation.Portrait
+        }
+        
+        // Metadata output
+        
+        var metadataOutput = createMetadataOutput()
+        if(captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+            // metadataOutput.metadataObjectTypes =
+        }
+        
+        captureSession.commitConfiguration()
+        dispatch_async(_serialQueue, { () -> Void in
+            captureSession.startRunning()
+        })
+        
+        _captureSession = captureSession;
     }
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func captureOutput(captureOutput: AVCaptureOutput, metadataObjects: NSArray, coonection:AVCaptureConnection) {
+        _facesMetadataObjects = metadataObjects
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
+        
+        let pixelBuffer: CVPixelBufferRef = CMSampleBufferGetImageBuffer(sampleBuffer)
+        
+        if(pixelBuffer != nil) {
+            let attachments: CFDictionaryRef = CMCopyDictionaryOfAttachments(kCFAllocatorDefault, sampleBuffer, kCMAttachmentMode_ShouldPropagate)
+            let ciImage = CIImage(CVPixelBuffer: pixelBuffer, options: attachments)
+            
+            if(attachments) {
+                CFRelease(attachments)
+            }
+            
+            let extent: CGRect = ciImage.extent()
+            
+        }
+    }
+    
+    // helpers
 
     func getCamera() -> AVCaptureDevice {
         
         let devices = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
-        
         for device in devices {
-            
             if (device.position == AVCaptureDevicePosition.Front) {
                 return device as AVCaptureDevice
             }
-            
         }
-        
         return AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
     }
     
     func createVideoDataOutput() -> AVCaptureVideoDataOutput {
         
         let videoDataOutput = AVCaptureVideoDataOutput()
-        // videoDataOutput.setSampleBufferDelegate(self, queue: serialQueue)
+        videoDataOutput.setSampleBufferDelegate(self, queue: _serialQueue)
         return videoDataOutput
+    }
+    
+    func createMetadataOutput() -> AVCaptureMetadataOutput {
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        metadataOutput.setMetadataObjectsDelegate(self, queue: _serialQueue)
+        return metadataOutput
+    }
+    
+    func metadataOutput(metadataOutput:AVCaptureMetadataOutput, allowedObjectTypes:NSArray) -> NSArray {
+       
+        var available = NSSet(array: metadataOutput.availableMetadataObjectTypes);
+        available.intersectsSet(NSSet(array:allowedObjectTypes))
+        return available.allObjects
+    }
+    
+    func faceMetaDataObjectTypes() -> NSArray {
+        
+        return [AVMetadataObjectTypeFace]
     }
 }
 
